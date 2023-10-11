e1d4f4158e3d16edf1abac318c803a4536cde617
arjun.sridhar12345 <arjun.sridhar1998@gmail.com>
2023-10-11 15:48:25 -0700

    add surface channel upload

--- src/np_codeocean/upload.py
+++ src/np_codeocean/upload.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import csv
 import pathlib
 import sys
+import datetime
 from pathlib import Path
 from typing import NamedTuple
 
@@ -18,14 +19,28 @@ logger = np_logging.get_logger(__name__)
 
 CONFIG = np_config.fetch('/projects/np_codeocean')
 
-class CodeOceanUpload(NamedTuple):
-    """Objects required for uploading a Mindscope Neuropixels session to CodeOcean.
+class CodeOceanSurfaceChannelUpload(NamedTuple):
+    """Objects required for uploading a Mindscope Neuropixels surface channel session to CodeOcean.
     
     Paths are symlinks to files on np-exp.
     """
     session: np_session.Session
     """Session object that the paths belong to."""
     
+    ephys: Path
+    """Directory of symlinks to raw ephys data files on np-exp, with only one
+    `recording` per `Record Node` folder."""
+
+    job: Path
+    """File containing job parameters for `aind-data-transfer`"""
+
+class CodeOceanUpload(NamedTuple):
+    """Objects required for uploading a Mindscope Neuropixels session to CodeOcean.
+        Paths are symlinks to files on np-exp.
+    """
+    session: np_session.Session
+    """Session object that the paths belong to."""
+
     behavior: Path
     """Directory of symlinks to files in top-level of session folder on np-exp,
     plus all files in `exp` subfolder, if present."""
@@ -36,7 +51,7 @@ class CodeOceanUpload(NamedTuple):
 
     job: Path
     """File containing job parameters for `aind-data-transfer`"""
-
+    
 
 def create_ephys_symlinks(session: np_session.Session, dest: Path) -> None:
     """Create symlinks in `dest` pointing to raw ephys data files on np-exp, with only one
@@ -86,6 +101,30 @@ def get_ephys_upload_csv_for_session(session: np_session.Session, ephys: Path, b
         'aws-param-store-name': CONFIG['aws-param-store-name'],
     } # type: ignore
 
+def get_surface_channel_start_time(session: np_session.Session) -> datetime.datetime:
+    sync_messages_path = tuple(session.npexp_path.glob('*/*/*/sync_messages.txt'))
+    if not sync_messages_path:
+        raise ValueError(f'No sync messages txt found for surface channel session {session}')
+    sync_messages_path = sync_messages_path[0]
+
+    with open(sync_messages_path, 'r') as f:
+        software_time_line = f.readlines()[0]
+
+    timestamp_value = float(software_time_line[software_time_line.index(':')+2:].strip())
+    timestamp = datetime.datetime.fromtimestamp(timestamp_value / 1e3)
+    return timestamp
+        
+def get_surface_channel_ephys_upload_csv_for_session(session: np_session.Session, ephys:Path) -> dict[str, str | int]:
+    return {
+        'data-source': np_config.normalize_path(ephys).as_posix(),
+        's3-bucket': CONFIG['s3-bucket'],
+        'subject-id': str(session.mouse),
+        'experiment-type': 'ecephys',
+        'modality': 'ECEPHYS',
+        'acq-date': f'{session.date:%Y-%m-%d}',
+        'acq-time': f'{get_surface_channel_start_time(session):%H-%M-%S}_surface_channels', # probably won't work but worth a shot
+        'aws-param-store-name': CONFIG['aws-param-store-name'],
+    } # type: ignore
 
 def create_upload_job(session: np_session.Session, job: Path, ephys: Path, behavior: Path) -> None:
     logger.info(f'Creating upload job file {job} for session {session}...')
@@ -95,6 +134,13 @@ def create_upload_job(session: np_session.Session, job: Path, ephys: Path, behav
         w.writerow(_csv.keys())
         w.writerow(_csv.values()) 
 
+def create_surface_channel_upload_job(session: np_session.Session, job: Path, ephys: Path) -> None:
+    logger.info(f'Creating upload job file {job} for session {session}...')
+    _csv = get_surface_channel_ephys_upload_csv_for_session(session, ephys)
+    with open(job, 'w') as f:
+        w = csv.writer(f)
+        w.writerow(_csv.keys())
+        w.writerow(_csv.values()) 
 
 def create_codeocean_upload(session: str | int | np_session.Session) -> CodeOceanUpload:
     """Create directories of symlinks to np-exp files with correct structure
@@ -120,10 +166,29 @@ def create_codeocean_upload(session: str | int | np_session.Session) -> CodeOcea
     create_upload_job(upload.session, upload.job, upload.ephys, upload.behavior)    
     return upload
 
+def create_codeocean_surface_channel_upload(session: str | int | np_session.Session) -> CodeOceanSurfaceChannelUpload:
+    session = np_session.Session(session)
+    root = np_session.NPEXP_PATH / 'codeocean' / f'{session.folder}_surface_channels'
+    logger.debug(f'Created directory {root} for Surface Channel CodeOcean upload')
+
+    upload = CodeOceanSurfaceChannelUpload(
+        session=session,
+        ephys = np_config.normalize_path(root / 'ephys'),
+        job = np_config.normalize_path(root / 'upload.csv'),
+    )
+    create_ephys_symlinks(upload.session, upload.ephys)
+    create_surface_channel_upload_job(upload.session, upload.job, upload.ephys)    
+    return upload
+
 
 def upload_session(session: str | int | pathlib.Path | np_session.Session) -> None:
     utils.ensure_credentials()
-    upload = create_codeocean_upload(str(session))
+
+    if 'surface' in str(session):
+        upload = create_codeocean_surface_channel_upload(str(session))
+    else:
+        upload = create_codeocean_upload(str(session))
+        
     np_logging.web('np_codeocean').info(f'Uploading {upload.session}')
     s3_upload_job.GenericS3UploadJobList(["--jobs-csv-file", upload.job.as_posix()]).run_job()
     np_logging.web('np_codeocean').info(f'Finished uploading {upload.session}')
@@ -132,4 +197,6 @@ def main() -> None:
     upload_session(sys.argv[1])
     
 if __name__ == '__main__':
-    main()
+    session = np_session.Session(r"\\allen\programs\mindscope\workgroups\dynamicrouting\PilotEphys\Task 2 pilot\DRpilot_660023_20230808_surface_channels")
+    get_surface_channel_start_time(session)
+    #main()
