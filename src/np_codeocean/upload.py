a08cc58fca76aef16da8fbdfa9fe658d024a7738
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-03-20 18:03:30 -0700

    Only test when running as script

--- src/np_codeocean/upload.py
+++ src/np_codeocean/upload.py
@@ -352,6 +352,5 @@ if __name__ == '__main__':
     import doctest
 
     doctest.testmod(
-        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE)
+        optionflags=(doctest.IGNORE_EXCEPTION_DETAIL | doctest.NORMALIZE_WHITESPACE),
     )
-    main()
