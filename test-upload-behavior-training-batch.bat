b1397e8badb67bb6a19bf5c07385b4b6d711ef45
corbennett <corbennett@gmail.com>
2024-05-28 17:56:18 -0700

    fix upload test scripts

--- test-upload-behavior-training-batch.bat
+++ test-upload-behavior-training-batch.bat
@@ -1,1 +1,1 @@
-pdm run python src/np_codeocean/scripts/upload_dynamic_routing_training.py --mode="batch" --test --debug --dry-run
+pdm run python src/np_codeocean/scripts/upload_dynamic_routing_training.py --mode="batch" --test --debug
