a48326b95de89e3cec8eed4f14c7951966f586ac
bjhardcastle <ben.hardcastle@alleninstitute.org>
2024-06-05 12:43:10 -0700

    Rename scripts

--- test-upload-behavior-training-batch.bat
+++ test-upload-behavior-training-batch.bat
@@ -1,1 +1,1 @@
-pdm run python src/np_codeocean/scripts/upload_dynamic_routing_training.py --mode="batch" --test --debug
+pdm run python src/np_codeocean/scripts/upload_dynamic_routing_behavior.py --mode="batch" --test --debug
