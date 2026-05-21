137e55096d5793ed37bdad67f659e06112b452a4
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-05-21 14:05:28 -0700

    Move logging to main (DR ephys)

--- src/np_codeocean/scripts/upload_dynamic_routing_ecephys.py
+++ src/np_codeocean/scripts/upload_dynamic_routing_ecephys.py
@@ -26,17 +26,7 @@ UPLOAD_ROOT = pathlib.Path(
     "//allen/programs/mindscope/workgroups/dynamicrouting/codeocean-upload"
 )
 (UPLOAD_ROOT / "logs").mkdir(parents=True, exist_ok=True)
-logging.basicConfig(
-    filename=(
-        UPLOAD_ROOT
-        / "logs"
-        / f"ephys_upload_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-    ).as_posix(),
-    level=logging.DEBUG,
-    force=True,
-    format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-)
+
 logger = logging.getLogger(__name__)
 
 
@@ -218,6 +208,17 @@ def parse_args() -> argparse.Namespace:
 
 
 def main() -> None:
+    logging.basicConfig(
+        filename=(
+            UPLOAD_ROOT
+            / "logs"
+            / f"ephys_upload_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        ).as_posix(),
+        level=logging.DEBUG,
+        force=True,
+        format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
     args = parse_args()
     kwargs = vars(args)
     np_codeocean.utils.set_npc_lims_credentials()
